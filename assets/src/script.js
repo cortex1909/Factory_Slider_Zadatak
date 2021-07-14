ef6e1eb512ef0860a94cbc9b951ef009bdec0336
cortex1909 <gladiatus330@gmail.com>
2021-07-14 18:44:35 +0200

    Addon

--- assets/src/script.js
+++ assets/src/script.js
@@ -3,37 +3,47 @@ const bottomImages = $('.bottom-row')
 const rightBtn = $('#right')
 const leftBtn = $('#left')
 
-
 $(rightBtn).click(() => {
+    $(rightBtn).prop('disabled', true)
     let lastImageTop = $(".top-row .img-slide").last()
     $(lastImageTop).clone().prependTo(topImages)
-/*     $(topImages).animate( {
-        scrollLeft: $(topImages).scrollLeft() - $('.top-row .img-slide').last().outerWidth(true)
-    }, 1000) */
+    $(topImages).fadeOut(500, function(){
+        $(topImages).fadeIn(500);
+    });
     $(lastImageTop).remove()
 
     let lastImageBottom = $(".bottom-row .img-slide").last()
     $(lastImageBottom).clone().prependTo(bottomImages)
-/*     $(bottomImages).animate( {
-        scrollLeft: $(bottomImages).scrollLeft() - $('.bottom-row .img-slide').last().outerWidth(true)
-    }, 1000) */
+    $(bottomImages).fadeOut(500, function(){
+        $(bottomImages).fadeIn(500);
+    });
     $(lastImageBottom).remove()
+    if ($(rightBtn).is(':animated')) {
+        $(rightBtn).prop('disabled', true)
+    };
+
+    setTimeout(() => {
+        $(rightBtn).prop('disabled', false)
+    }, 1000)
 })
 
 $(leftBtn).click(() => {
+    $(leftBtn).prop('disabled', true)
     let firstImageTop = $(".top-row .img-slide").first()
-/*     $(topImages).animate( {
-        scrollLeft: $(topImages).scrollLeft() + $('.top-row .img-slide').last().outerWidth(true)
-    }, 1000) */
+    $(topImages).fadeOut('fast', function(){
+        $(topImages).fadeIn();
+    });
     $(firstImageTop).clone().appendTo(topImages)
     $(firstImageTop).remove()
 
     let firstImageBottom = $(".bottom-row .img-slide").first()
-/*     $(bottomImages).animate( {
-        scrollLeft: $(bottomImages).scrollLeft() + $('.bottom-row .img-slide').last().outerWidth(true)
-    }, 1000) */
+    $(bottomImages).fadeOut('fast', function(){
+        $(bottomImages).fadeIn();
+    });
     $(firstImageBottom).clone().appendTo(bottomImages)
     $(firstImageBottom).remove()
+    
+    setTimeout(() => {
+        $(leftBtn).prop('disabled', false)
+    }, 1000)
 })
-
-// Nisam pronašao način kako iskoristiti animate, kako bih kroz animaciju u trajanju 1s pomaknuo cijeli red slika u lijevo, odnosno u desno. Pokušaj zakomentiran gore.
