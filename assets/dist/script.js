4b0d21b47045df97e4677d9a14a2585c60045f35
cortex1909 <gladiatus330@gmail.com>
2021-07-15 09:12:44 +0200

    Update script.js

--- assets/dist/script.js
+++ assets/dist/script.js
@@ -5,34 +5,44 @@ var bottomImages = $('.bottom-row');
 var rightBtn = $('#right');
 var leftBtn = $('#left');
 $(rightBtn).click(function () {
+  $(rightBtn).prop('disabled', true);
   var lastImageTop = $(".top-row .img-slide").last();
   $(lastImageTop).clone().prependTo(topImages);
-  /*     $(topImages).animate( {
-          scrollLeft: $(topImages).scrollLeft() - $('.top-row .img-slide').last().outerWidth(true)
-      }, 1000) */
-
+  $(topImages).fadeOut(500, function () {
+    $(topImages).fadeIn(500);
+  });
   $(lastImageTop).remove();
   var lastImageBottom = $(".bottom-row .img-slide").last();
   $(lastImageBottom).clone().prependTo(bottomImages);
-  /*     $(bottomImages).animate( {
-          scrollLeft: $(bottomImages).scrollLeft() - $('.bottom-row .img-slide').last().outerWidth(true)
-      }, 1000) */
-
+  $(bottomImages).fadeOut(500, function () {
+    $(bottomImages).fadeIn(500);
+  });
   $(lastImageBottom).remove();
+
+  if ($(rightBtn).is(':animated')) {
+    $(rightBtn).prop('disabled', true);
+  }
+
+  ;
+  setTimeout(function () {
+    $(rightBtn).prop('disabled', false);
+  }, 1000);
 });
 $(leftBtn).click(function () {
+  $(leftBtn).prop('disabled', true);
   var firstImageTop = $(".top-row .img-slide").first();
-  /*     $(topImages).animate( {
-          scrollLeft: $(topImages).scrollLeft() + $('.top-row .img-slide').last().outerWidth(true)
-      }, 1000) */
-
+  $(topImages).fadeOut(500, function () {
+    $(topImages).fadeIn(500);
+  });
   $(firstImageTop).clone().appendTo(topImages);
   $(firstImageTop).remove();
   var firstImageBottom = $(".bottom-row .img-slide").first();
-  /*     $(bottomImages).animate( {
-          scrollLeft: $(bottomImages).scrollLeft() + $('.bottom-row .img-slide').last().outerWidth(true)
-      }, 1000) */
-
+  $(bottomImages).fadeOut(500, function () {
+    $(bottomImages).fadeIn(500);
+  });
   $(firstImageBottom).clone().appendTo(bottomImages);
   $(firstImageBottom).remove();
-}); // Nisam pronašao način kako iskoristiti animate, kako bih kroz animaciju u trajanju 1s pomaknuo cijeli red slika u lijevo, odnosno u desno. Pokušaj zakomentiran gore.
+  setTimeout(function () {
+    $(leftBtn).prop('disabled', false);
+  }, 1000);
+});
