05fb4486ebfd1c460a73293d4baa1ba81429d971
cortex1909 <gladiatus330@gmail.com>
2021-07-21 16:34:05 +0200

    Korekcija
    21.07.

--- assets/dist/script.js
+++ assets/dist/script.js
@@ -1,48 +1,38 @@
 "use strict";
 
-var topImages = $('.top-row');
-var bottomImages = $('.bottom-row');
-var rightBtn = $('#right');
-var leftBtn = $('#left');
-$(rightBtn).click(function () {
-  $(rightBtn).prop('disabled', true);
-  var lastImageTop = $(".top-row .img-slide").last();
-  $(lastImageTop).clone().prependTo(topImages);
-  $(topImages).fadeOut(500, function () {
-    $(topImages).fadeIn(500);
-  });
-  $(lastImageTop).remove();
-  var lastImageBottom = $(".bottom-row .img-slide").last();
-  $(lastImageBottom).clone().prependTo(bottomImages);
-  $(bottomImages).fadeOut(500, function () {
-    $(bottomImages).fadeIn(500);
-  });
-  $(lastImageBottom).remove();
-
-  if ($(rightBtn).is(':animated')) {
+$(document).ready(function () {
+  var topImages = $('.top-row');
+  var bottomImages = $('.bottom-row');
+  var rightBtn = $('#right');
+  var leftBtn = $('#left');
+  $(rightBtn).click(function () {
     $(rightBtn).prop('disabled', true);
-  }
+    var lastImageTop = $(".top-row .img-slide").last();
+    $(lastImageTop).clone().prependTo(topImages);
+    $(lastImageTop).remove();
+    var lastImageBottom = $(".bottom-row .img-slide").last();
+    $(lastImageBottom).clone().prependTo(bottomImages);
+    $(lastImageBottom).remove();
 
-  ;
-  setTimeout(function () {
-    $(rightBtn).prop('disabled', false);
-  }, 1000);
-});
-$(leftBtn).click(function () {
-  $(leftBtn).prop('disabled', true);
-  var firstImageTop = $(".top-row .img-slide").first();
-  $(topImages).fadeOut(500, function () {
-    $(topImages).fadeIn(500);
+    if ($(rightBtn).is(':animated')) {
+      $(rightBtn).prop('disabled', true);
+    }
+
+    ;
+    setTimeout(function () {
+      $(rightBtn).prop('disabled', false);
+    }, 500);
   });
-  $(firstImageTop).clone().appendTo(topImages);
-  $(firstImageTop).remove();
-  var firstImageBottom = $(".bottom-row .img-slide").first();
-  $(bottomImages).fadeOut(500, function () {
-    $(bottomImages).fadeIn(500);
+  $(leftBtn).click(function () {
+    $(leftBtn).prop('disabled', true);
+    var firstImageTop = $(".top-row .img-slide").first();
+    $(firstImageTop).clone().appendTo(topImages);
+    $(firstImageTop).remove();
+    var firstImageBottom = $(".bottom-row .img-slide").first();
+    $(firstImageBottom).clone().appendTo(bottomImages);
+    $(firstImageBottom).remove();
+    setTimeout(function () {
+      $(leftBtn).prop('disabled', false);
+    }, 500);
   });
-  $(firstImageBottom).clone().appendTo(bottomImages);
-  $(firstImageBottom).remove();
-  setTimeout(function () {
-    $(leftBtn).prop('disabled', false);
-  }, 1000);
 });
--- assets/src/script.js
+++ assets/src/script.js
@@ -1,49 +1,35 @@
-const topImages = $('.top-row')
-const bottomImages = $('.bottom-row')
-const rightBtn = $('#right')
-const leftBtn = $('#left')
+$(document).ready(function () {
+    const topImages = $('.top-row')
+    const bottomImages = $('.bottom-row')
+    const rightBtn = $('#right')
+    const leftBtn = $('#left')
 
-$(rightBtn).click(() => {
-    $(rightBtn).prop('disabled', true)
-    let lastImageTop = $(".top-row .img-slide").last()
-    $(lastImageTop).clone().prependTo(topImages)
-    $(topImages).fadeOut(500, function(){
-        $(topImages).fadeIn(500);
-    });
-    $(lastImageTop).remove()
-
-    let lastImageBottom = $(".bottom-row .img-slide").last()
-    $(lastImageBottom).clone().prependTo(bottomImages)
-    $(bottomImages).fadeOut(500, function(){
-        $(bottomImages).fadeIn(500);
-    });
-    $(lastImageBottom).remove()
-    if ($(rightBtn).is(':animated')) {
+    $(rightBtn).click(() => {
         $(rightBtn).prop('disabled', true)
-    };
+        let lastImageTop = $(".top-row .img-slide").last()
+        $(lastImageTop).clone().prependTo(topImages)
+        $(lastImageTop).remove()
+        let lastImageBottom = $(".bottom-row .img-slide").last()
+        $(lastImageBottom).clone().prependTo(bottomImages)
+        $(lastImageBottom).remove()
+        if ($(rightBtn).is(':animated')) {
+            $(rightBtn).prop('disabled', true)
+        };
+        setTimeout(() => {
+            $(rightBtn).prop('disabled', false)
+        }, 500)
+    })
 
-    setTimeout(() => {
-        $(rightBtn).prop('disabled', false)
-    }, 1000)
-})
-
-$(leftBtn).click(() => {
-    $(leftBtn).prop('disabled', true)
-    let firstImageTop = $(".top-row .img-slide").first()
-    $(topImages).fadeOut(500, function(){
-        $(topImages).fadeIn(500);
-    });
-    $(firstImageTop).clone().appendTo(topImages)
-    $(firstImageTop).remove()
-
-    let firstImageBottom = $(".bottom-row .img-slide").first()
-    $(bottomImages).fadeOut(500, function(){
-        $(bottomImages).fadeIn(500);
-    });
-    $(firstImageBottom).clone().appendTo(bottomImages)
-    $(firstImageBottom).remove()
-
-    setTimeout(() => {
-        $(leftBtn).prop('disabled', false)
-    }, 1000)
-})
+    $(leftBtn).click(() => {
+        $(leftBtn).prop('disabled', true)
+        let firstImageTop = $(".top-row .img-slide").first()
+        $(firstImageTop).clone().appendTo(topImages)
+        $(firstImageTop).remove()
+        let firstImageBottom = $(".bottom-row .img-slide").first()
+        $(firstImageBottom).clone().appendTo(bottomImages)
+        $(firstImageBottom).remove()
+        setTimeout(() => {
+            $(leftBtn).prop('disabled', false)
+        }, 500)
+    })
+});
